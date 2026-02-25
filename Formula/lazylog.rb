c33bdde1642a594f29cb189b63345d53c9089a3f
ruitian <ruitian.y@bytedance.com>
2026-02-25 14:20:34 +0800

    update formula

--- Formula/lazylog.rb
+++ Formula/lazylog.rb
@@ -1,21 +1,19 @@
 class Lazylog < Formula
-  desc "Internal CLI tool lazylog"
-  homepage "https://code.byted.org/ies/lazylog"
+  desc "Terminal-based log viewer"
+  homepage "https://github.com/tr-nc/lazylog"
+  version "0.5.3"
 
-  url "https://code.byted.org/ies/lazylog.git", branch: "release"
-
-  version "0.5.1"
-
-  depends_on "rust" => :build
+  # Points to the precompiled binary in the GitHub releases panel
+  url "https://github.com/tr-nc/lazylog/releases/download/v#{version}/zl-macos.tar.gz"
+  
+  # IMPORTANT: You must calculate and insert the actual SHA256 hash of the zl-macos.tar.gz file.
+  # You can find this by running: shasum -a 256 zl-macos.tar.gz
+  sha256 "ce3f6c4a9ac967b6ddd3c5179a24f52f7059b711943c9f806ec729a2336457a3"
 
   def install
-    ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-
-    # Compiles the specific package from the downloaded workspace
-    system "cargo", "build", "--release", "--package", "lazylog"
-
-    # Moves the compiled binary into Homebrew's bin folder
-    bin.install "target/release/zl"
+    # Homebrew automatically downloads and extracts the tar.gz archive.
+    # We just need to install the extracted 'zl' binary into the bin folder.
+    bin.install "zl"
   end
 
   test do
