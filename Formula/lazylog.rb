b36917088a73e327acdcee07947ea3037a145618
Ruitian Yang 杨瑞天 <ruitian0716@gmail.com>
2026-02-25 16:54:27 +0800

    lazylog 0.6.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/lazylog.rb
+++ Formula/lazylog.rb
@@ -1,14 +1,14 @@
 class Lazylog < Formula
   desc "Terminal-based log viewer"
   homepage "https://github.com/tr-nc/lazylog"
-  version "0.6.0"
+  version "0.6.1"
 
   # Points to the precompiled binary in the GitHub releases panel
-  url "https://github.com/tr-nc/lazylog/releases/download/v0.6.0/lazylog-macos.tar.gz"
+  url "https://github.com/tr-nc/lazylog/releases/download/v0.6.1/lazylog-macos.tar.gz"
   
   # IMPORTANT: You must calculate and insert the actual SHA256 hash of the lazylog-macos.tar.gz file.
   # You can find this by running: shasum -a 256 lazylog-macos.tar.gz
-  sha256 "4f504b1c44c283fb000861e2992810a4463b80a3918ec37ebd39f9c869260853"
+  sha256 "4ba1f73615cde832a1427b10ca94f388f19351587a949be52b3127fcef6c1d27"
 
   def install
     # Install the main binary
